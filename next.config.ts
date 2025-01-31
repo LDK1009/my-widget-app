e8a7ce5d9d3996dee16335d309129e4b8ed00930
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2025-01-31 12:39:41 +0900

    커밋 메시지

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,13 @@
-import type { NextConfig } from "next";
-
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  sw: "service-worker.js",
 });
 
-module.exports = withPWA({
-  reactStrictMode: true,
-});
-
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+   // ...기존에 추가했었던 next.config.ts 설정
 };
 
-module.exports = withPWA({
-  reactStrictMode: true,
-});
-
-export default nextConfig;
-
+module.exports = withPWA(nextConfig);
